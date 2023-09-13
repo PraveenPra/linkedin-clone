20a8bf02b46ffd045b746236986aa777129199b2
PraveenPra <prvp6501@gmail.com>
2023-09-13 23:56:39 +0530

    Added proper links from unsplash for connections images

--- src/pages/my-network.jsx
+++ src/pages/my-network.jsx
@@ -50,91 +50,91 @@ const connectionData = [
     role: "AI Intern",
     mutualConnections: 13,
     profileImage: "https://source.unsplash.com/random/200x200/?profile&1",
-    bannerImage: "https://source.unsplash.com/random/1024x768/?banner"
+    bannerImage: "https://source.unsplash.com/random/1024x768/?banner&1"
   },
   {
     name: "John Doe",
     role: "Software Engineer",
     mutualConnections: 8,
     profileImage: "https://source.unsplash.com/random/200x200/?profile&2",
-    bannerImage: "https://source.unsplash.com/random/1024x768/?banner"
+    bannerImage: "https://source.unsplash.com/random/1024x768/?banner&2"
   },
   {
     name: "Jane Smith",
     role: "Data Scientist",
     mutualConnections: 5,
-    profileImage: "https://source.unsplash.com/random/200x200/?profile",
-    bannerImage: "https://source.unsplash.com/random/1024x768/?banner"
+    profileImage: "https://source.unsplash.com/random/200x200/?profile&3",
+    bannerImage: "https://source.unsplash.com/random/1024x768/?banner&3"
   },
   {
     name: "Michael Johnson",
     role: "Product Manager",
     mutualConnections: 20,
-    profileImage: "https://source.unsplash.com/random/200x200/?profile",
-    bannerImage: "https://source.unsplash.com/random/1024x768/?banner"
+    profileImage: "https://source.unsplash.com/random/200x200/?profile&4",
+    bannerImage: "https://source.unsplash.com/random/1024x768/?banner&4"
   },
   {
     name: "Sarah Davis",
     role: "UX Designer",
     mutualConnections: 15,
-    profileImage: "https://source.unsplash.com/random/200x200/?profile",
-    bannerImage: "https://source.unsplash.com/random/1024x768/?banner"
+    profileImage: "https://source.unsplash.com/random/200x200/?profile&5",
+    bannerImage: "https://source.unsplash.com/random/1024x768/?banner&5"
   },
   {
     name: "David Lee",
     role: "Data Analyst",
     mutualConnections: 7,
-    profileImage: "https://source.unsplash.com/random/200x200/?profile",
-    bannerImage: "https://source.unsplash.com/random/1024x768/?banner"
+    profileImage: "https://source.unsplash.com/random/200x200/?profile&6",
+    bannerImage: "https://source.unsplash.com/random/1024x768/?banner&6"
   },
   {
     name: "Emily White",
     role: "Marketing Specialist",
     mutualConnections: 11,
-    profileImage: "https://source.unsplash.com/random/200x200/?profile",
-    bannerImage: "https://source.unsplash.com/random/1024x768/?banner"
+    profileImage: "https://source.unsplash.com/random/200x200/?profile&7",
+    bannerImage: "https://source.unsplash.com/random/1024x768/?banner&7"
   },
   {
     name: "Alex Wilson",
     role: "Frontend Developer",
     mutualConnections: 9,
-    profileImage: "https://source.unsplash.com/random/200x200/?profile",
-    bannerImage: "https://source.unsplash.com/random/1024x768/?banner"
+    profileImage: "https://source.unsplash.com/random/200x200/?profile&8&9",
+    bannerImage: "https://source.unsplash.com/random/1024x768/?banner&8&9"
   },
   {
     name: "Olivia Brown",
     role: "Project Manager",
     mutualConnections: 14,
-    profileImage: "https://source.unsplash.com/random/200x200/?profile",
-    bannerImage: "https://source.unsplash.com/random/1024x768/?banner"
+    profileImage: "https://source.unsplash.com/random/200x200/?profile&9",
+    bannerImage: "https://source.unsplash.com/random/1024x768/?banner&9"
   },
   {
     name: "Daniel Clark",
     role: "Software Architect",
     mutualConnections: 18,
-    profileImage: "https://source.unsplash.com/random/200x200/?profile",
-    bannerImage: "https://source.unsplash.com/random/1024x768/?banner"
+    profileImage: "https://source.unsplash.com/random/200x200/?profile&10",
+    bannerImage: "https://source.unsplash.com/random/1024x768/?banner&10"
   },
   {
     name: "Sophia Martinez",
     role: "Data Engineer",
     mutualConnections: 6,
-    profileImage: "https://source.unsplash.com/random/200x200/?profile",
-    bannerImage: "https://source.unsplash.com/random/1024x768/?banner"
+    profileImage: "https://source.unsplash.com/random/200x200/?profile&11",
+    bannerImage: "https://source.unsplash.com/random/1024x768/?banner&11"
   },
   {
     name: "Matthew ",
     role: "Sales Manager",
     mutualConnections: 12,
-    profileImage: "https://source.unsplash.com/random/200x200/?profile",
-    bannerImage: "https://source.unsplash.com/random/1024x768/?banner"
+    profileImage: "https://source.unsplash.com/random/200x200/?profile&12",
+    bannerImage: "https://source.unsplash.com/random/1024x768/?banner&12"
   },
   {
     name: "Ella Thomas",
     role: "Graphic Designer",
     mutualConnections: 10,
-    profileImage: "https://source.unsplash.com/random/200x200/?profile",
-    bannerImage: "https://source.unsplash.com/random/1024x768/?banner"
+    profileImage: "https://source.unsplash.com/random/200x200/?profile&13",
+    bannerImage: "https://source.unsplash.com/random/1024x768/?banner&13"
   },
 ];
 
