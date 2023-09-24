fea2042a39054451bb27073cd2f81f506228de03
PraveenPra <prvp6501@gmail.com>
2023-09-24 23:54:50 +0530

    Making my network connection responsive

--- src/pages/my-network.jsx
+++ src/pages/my-network.jsx
@@ -2,7 +2,7 @@ import TwoColumnLayout from '@/components/Layouts/TwoColumnLayout'
 import IconMenu from '@/components/MenuComponents/iconMenu'
 import BanneredImage from '@/components/ImageComponents/banneredImage'
 import React from 'react'
-
+import sx from './my-network.module.css'
 import { CgUserAdd } from 'react-icons/cg';
 import EmptyAvatar from '@/components/ImageComponents/emptyAvatar';
 import ConnectionItem from '@/components/connectionItem';
@@ -167,12 +167,7 @@ MyNetwork.getLayout = function getLayout(page) {
       </div>
 
 
-      <section style={{
-
-        display: 'grid',
-        gap: '8px',
-        gridTemplateColumns: 'repeat(4,1fr)',
-      }}>
+      <section className={sx.connGrid}>
 
         {connectionData?.map((item, i) => {
           return <ConnectionItem key={i} data={item} />
